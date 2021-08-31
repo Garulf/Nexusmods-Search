ad191564a8dfcbf118f47c9e5779e4b58965d544
Garulf <535299+Garulf@users.noreply.github.com>
2021-08-31 09:59:21 -0400

    skeleton

---
+++ plugin/nexusmods-search.py
@@ -0,0 +1,12 @@
+from flox import Flox
+
+class Nexusmods-Search(Flox):
+
+    def query(self, query):
+        pass
+
+    def context(self, data):
+        pass
+
+if __name__ == "__main__":
+    Nexusmods-Search()
